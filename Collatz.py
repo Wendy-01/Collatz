fa5eefb4b370e3d55358eed8e444dceecaf3d55f
Wendy Wang <wendyw@synnex.com>
2019-10-21 17:37:48 +0800

    Add Collatz py file to git

---
+++ Collatz.py
@@ -0,0 +1,25 @@
+def collatz(number):
+    if number % 2 == 0:
+        return (number//2)
+    else:
+        return (number * 3 + 1)
+
+
+try:
+
+    input_num = int(input("Your Number Please:"))
+except ValueError:
+    print( '请输入整数')
+    non_int = "输入整数"
+
+while True:
+    input_num = int(input("Your Number Please:"))
+    if non_int != "请输入整数":
+        break
+
+while True:
+    input_num = collatz(input_num)
+    print (input_num)
+    if input_num == 1:
+        break
+print("Game over!")
