c92033c30aeed031185d5ab21cf0fa803be684d3
Wendy Wang <wendyw@synnex.com>
2019-10-22 09:19:37 +0800

    disable ask user to input int32 for many times

--- Collatz.py
+++ Collatz.py
@@ -5,21 +5,21 @@ def collatz(number):
         return (number * 3 + 1)
 
 
-try:
+if __name__ == "__main__":
+    try:
+        input_num = int(input("Your Number Please:"))
+    except ValueError:
+        input_num = int(input("请输入整数:"))
+        #non_int = "输入整数"
 
-    input_num = int(input("Your Number Please:"))
-except ValueError:
-    print( '请输入整数')
-    non_int = "输入整数"
+    #while True:
+        #input_num = int(input("Your Number Please:"))
+        #if non_int != "输入整数":
+          #  break
 
-while True:
-    input_num = int(input("Your Number Please:"))
-    if non_int != "请输入整数":
-        break
-
-while True:
-    input_num = collatz(input_num)
-    print (input_num)
-    if input_num == 1:
-        break
-print("Game over!")
+    while True:
+        input_num = collatz(input_num)
+        print (input_num)
+        if input_num == 1:
+            break
+    print("Game over!")
